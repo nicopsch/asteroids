33f8b70ba0c238d952380bf8be4034f9a73c087c
nicop <nico.przywarra@gmx.net>
2025-08-23 09:36:18 +0200

    Player + Hitbox

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
+
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    pygame.init
+    pygame.init()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0), rect=None, special_flags=0)
+        player.draw(screen)
+        dt = Clock.tick(60) / 1000
         pygame.display.flip()
 
 if __name__ == "__main__":
